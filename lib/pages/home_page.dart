b26f1460ec5cd751dc926d39fc111bc35831264f
daodaoliang <geekpai.dev@gmail.com>
2019-06-20 00:18:02 +0800

    1.HomePage增加轮播Banner插件

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,11 +7,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _imageUrl = [
+    'http://img.zcool.cn/community/01ce8b582439aea84a0e282ba855d9.jpg',
+    'http://img.zcool.cn/community/01f2ac576df0980000018c1b1cb5e1.jpg',
+    'http://img.zcool.cn/community/01f0305541db6800000115417f56ae.jpg@1280w_1l_2o_100sh.jpg'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('首页'),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 160,
+              // 轮播插件
+              child: Swiper(
+                itemCount: _imageUrl.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(
+                    _imageUrl[index],
+                    fit: BoxFit.fill,
+                  );
+                },
+                pagination: SwiperPagination(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
