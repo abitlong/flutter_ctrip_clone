e5231fd0a5c29e7959511370047204c67cab9b76
daodaoliang <geekpai.dev@gmail.com>
2019-06-21 01:05:00 +0800

    3.在搜索页发起Http请求

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class SearchPage extends StatefulWidget {
   @override
@@ -6,12 +9,62 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  String showResult = '';
+
+  //发起请求
+  Future<CommonModel> fetchPost() async {
+    final response = await http
+        .get('http://www.devio.org/io/flutter_app/json/test_common_model.json');
+    final result = json.decode(response.body);
+    return CommonModel.fromJson(result);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('搜索'),
-      ),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('HTTP'),
+          ),
+          body: Column(
+            children: <Widget>[
+              InkWell(
+                onTap: () {
+                  fetchPost().then((CommonModel value) {
+                    setState(() {
+                      showResult =
+                          '请求结果：\n hideAppBar: ${value.hideAppBar} \n icon: ${value.icon}';
+                    });
+                  });
+                },
+                child: Text(
+                  '点我请求',
+                  style: TextStyle(fontSize: 26),
+                ),
+              ),
+              Text(showResult)
+            ],
+          )),
     );
+  }
+}
+
+class CommonModel {
+  final String icon;
+  final String title;
+  final String url;
+  final String statusBarColor;
+  final bool hideAppBar;
+
+  CommonModel(
+      {this.icon, this.title, this.url, this.statusBarColor, this.hideAppBar});
+
+  factory CommonModel.fromJson(Map<String, dynamic> json) {
+    return CommonModel(
+        icon: json['icon'],
+        title: json['title'],
+        url: json['url'],
+        statusBarColor: json['statusBarColor'],
+        hideAppBar: json['hideAppBar']);
   }
 }
